dff1f40a0ba2a6c1f978079538a7104616e346e3
iecurius <saahilyechuri@gmail.com>
2019-02-15 13:34:54 -0500

    User class

---
+++ (Concept-Based) SystemV2/src/User.java
@@ -0,0 +1,10 @@
+public class User {
+    private static User ourInstance = new User();
+
+    public static User getInstance() {
+        return ourInstance;
+    }
+
+    private User() {
+    }
+}
