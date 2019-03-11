6003933cd7b2a183793e41dee108045a69273d6d
Sangvi98 <sanghavig@gatech.edu>
2019-03-10 23:29:12 -0400

    reading in multiple users

--- (Concept-Based) SystemV2/src/main.java
+++ (Concept-Based) SystemV2/src/main.java
@@ -8,25 +8,31 @@ public class main {
 
         File file = new File("./src/users.csv");
         Scanner userFile = new Scanner(file);
-        String userLine = userFile.nextLine();
-        String delimiter = ",";
-        String[] userInfo = userLine.split(delimiter);
 
-        //Subgoal 1: picking the restaurant for the user to go to
+        while (userFile.hasNext()) {
+            String userLine = userFile.nextLine();
+            String delimiter = ",";
+            String[] userInfo = userLine.split(delimiter);
 
-        //this method call has the user filter out its own preferences
-        User user1 = new User(userInfo);
-        System.out.println("Main: Calling User to filter everything");
-        user1.filterEverything();
+            //Subgoal 1: picking the restaurant for the user to go to
 
-        //System.out.println(user1.restaurantChosen);
+            //this method call has the user filter out its own preferences
+            User user1 = new User(userInfo);
+            System.out.println("Main: Calling User to filter everything");
+            user1.filterEverything();
 
-        System.out.println("Main: Choosing Restaurant");
-        Food eatThis = user1.restaurantChosen.chooseFoodItem(user1.nutritionPreference, user1.budget, user1.dietPref, user1.hunger);
-        if (eatThis == null) {
-            System.out.println("Come back later. There's nothing available for you right now.");
-        } else {
-            System.out.println("Go eat some " + eatThis + " at " + user1.restaurantList.get(0));
+            //System.out.println(user1.restaurantChosen);
+
+            System.out.println("Main: Choosing Restaurant");
+            Food eatThis = user1.restaurantChosen.chooseFoodItem(user1.nutritionPreference, user1.budget, user1.dietPref, user1.hunger);
+            if (eatThis == null) {
+                System.out.println("Come back later. There's nothing available for you right now.");
+            } else {
+                System.out.println("Go eat some " + eatThis + " at " + user1.restaurantList.get(0));
+            }
+
+            System.out.println("-------------------------------------------");
         }
+
     }
 }
